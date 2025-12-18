f9ac920171c1d47e028613208779644981025b92
PurviLunawat9 <purvilunawat@gmail.com>
2025-12-18 19:28:03 +0530

    Added biofuel website files

---
+++ script.js
@@ -0,0 +1,7 @@
+function requestQuote() {
+  alert("Thank you! We'll contact you shortly for your bulk quote.");
+}
+
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
